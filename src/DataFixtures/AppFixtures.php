8db33cb21324d0a31a2f8490e870fab866b08995
Vadym Bondarenko <v.bondarenko2991@gmail.com>
2021-09-26 23:41:05 +0300

    redis

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
+    private PasswordHasherFactoryInterface $passwordHasherFactory;
+
+    public function __construct(PasswordHasherFactoryInterface $passwordHasherFactory)
+    {
+        $this->passwordHasherFactory = $passwordHasherFactory;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User(
+            'v.bondarenko2991@gmail.com',
+            ['ROLE_USER'],
+            $this->passwordHasherFactory->getPasswordHasher(User::class)->hash('pass1234'),
+        );
+        $manager->persist($user);
 
         $manager->flush();
     }
